92a14b731acc05f844c8adf38f7113e365cc22a1
margb13 <marg.b13@hotmail.com>
2018-03-27 10:20:29 -0500

    Deshabilitando boton y avance de validaciones

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,15 +18,29 @@
 //= require materialize-sprockets
 //= require_tree .
 
-
-$(document).on('turbolinks:load', function() {
-  $('.dropdown-button').dropdown();
-  $('select').material_select();
+$(document).on("turbolinks:load", function() {
+  $(".dropdown-button").dropdown();
+  $("select").material_select();
 });
 
+$(document).ready(function() {
+  $(".slider").slider();
 
-$(document).ready(function(){
-  $('.slider').slider();
+  /* -------- Deshabilitar elementos al cargar la vista------- */
+  $("#btn-login").prop("disabled", true);
 
+   /* -------- Validando inputs------- */
+  // Expresion regular que comprueba nombre propio y que empiecen por vocal si ésta va acentuada
+  emailRegex = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/
+
+  $('#email').keyup(function () {
+    var email = this.value;
+    // test para realizar prueba si valor del input coincide con el patron . Devuelve verdadero o falso
+    if (emailRegex.test(email)) {
+      return true;
+    } else {
+
+      return false;
+    }
+  });
 });
-    
